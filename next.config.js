7b44781c75ef2f7bec691de882f681008aee1346
PANDA Developer <you@example.com>
2026-07-24 02:38:57 +0300

    Fix GitHub Pages base path

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repoName = "panda-web";
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
   images: {
     unoptimized: true,
   },
