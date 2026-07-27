ccd44e9aacc53e885321ce86c73dcc8e2c05318f
PANDA Developer <you@example.com>
2026-07-27 15:43:46 +0300

    Add multi-image product gallery

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import "../styles/globals.css";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="D7Kg-W89aoy6ytFXFSkXWsjydTdZpdPY5dHMh4BY7ik" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
